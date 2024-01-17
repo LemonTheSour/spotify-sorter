1ef29e783377424b07692bbb53a0c5dc0e097ffc
Michael <m.m.downing@bigpond.com>
2024-01-17 22:57:02 +0800

    base tutorial completed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import os
 import base64
 import json
-from requests import post
+from requests import post, get
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -27,5 +27,36 @@ def get_token():
     return_token = json_result["access_token"]
     return return_token
 
+def get_auth_header(token):
+    """Function which constructs a header for post requests"""
+    return {"Authorization": "Bearer " + token}
+
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"q={artist_name}&type=artist&limit=1"
+
+    query_url = url + "?" + query
+    result = get(query_url, headers=headers, timeout=3.00)
+    json_result = json.loads(result.content)["artists"]["items"]
+    if len(json_result) == 0:
+        print("No artist with this name exists...")
+        return None
+
+    return json_result[0]
+
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+
 token = get_token()
-print(token)
+result = search_for_artist(token, "ACDC")
+artist_id = result["id"]
+songs = get_songs_by_artist(token, artist_id)
+
+for idx, song in enumerate(songs):
+    print(f"{idx + 1}. {song['name']}")
